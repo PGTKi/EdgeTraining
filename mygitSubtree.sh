40365ddc3ea3eab98fd79ae8ea75e7ae9678c052
yangcyself <13520183356@139.com>
2019-03-22 20:28:36 +0800

    added new scripts

--- mygitSubtree.sh
+++
@@ -1,8 +0,0 @@
-if [ "$1" = "pull" ]; then
-	echo git pull 
-	git subtree pull -P downSamplingTraining https://github.com/yangcyself/SDPoint.git master	
-elif [ "$1" = "push" ]; then
-	echo git push
-	git subtree push -P downSamplingTraining https://github.com/yangcyself/SDPoint.git master
-fi
-
