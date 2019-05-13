0124dc760b10e2a3f534b076cbca617ba0652107
Fuyuan Lyu Tommy <lfy_1997@126.com>
2019-05-13 17:12:38 +0800

    Update op-test-conv2d-sparse.py

--- Code/edge-tvm/testing-code/op-test/op-test-conv2d-sparse.py
+++ Code/edge-tvm/testing-code/op-test/op-test-conv2d-sparse.py
@@ -1,4 +1,3 @@
-import numpy as np
 import nnvm
 import nnvm.symbol as sym
 from nnvm.testing.check_computation import check_function
@@ -6,14 +5,24 @@ from nnvm import graph
 import numpy as np
 import time
 import tvm
-from nnvm.testing.utils import create_workload
+from nnvm.testing.utils import create_sparse_workload
+from tvm.contrib import graph_runtime
+
+# Hyper-parameter define
+batch_size = 1
+num_class = 10
+image_shape = (3, 32, 32)
+data_shape = (batch_size,) + image_shape
+out_shape = (batch_size, num_class)
+sparse_kernel_shape = (batch_size, 3, 32)
+dtype = "float32"
 
 data = sym.Variable("data")
-#conv_kernel = sym.Variable("conv_kernel")
-#conv_bias = sym.Variable("conv_bias")
-sparse_kernel = sym.Variable("sparse_kernel")
-#y = sym.conv2d_sparse(data=x, weight=conv_kernel, sparsity=sparse_kernel, bias=conv_bias, channels=1, kernel_size=(3,3), padding=(0,0), use_bias=True, out_layout='NCHW')
+sparse_kernel = sym.Variable("sparse_kernel", init=np.random.randint(0, 1, sparse_kernel_shape))
 y = sym.conv2d_sparse(data=data, sparsity=sparse_kernel, channels=1, kernel_size=(3,3), padding=(0,0), use_bias=False, out_layout='NCHW')
+y = sym.flatten(y)
+y = sym.dense(y, units=10)
+y =sym.softmax(y)
 out = y
 
 # Test Graph compilation
@@ -27,80 +36,34 @@ print("--------------Ends-----------------")
 
 
 # Create workload
-batch_size = 1
-num_class = 10
-image_shape = (3, 32, 32)
-data_shape = (batch_size,) + image_shape
-out_shape = (batch_size, num_class)
-dtype = "float32"
-net, params = create_workload(out, batch_size, image_shape, dtype)
-print("-------------Starts----------------")
+net, params = create_sparse_workload(out, batch_size, image_shape, dtype)
+print("-------------Starts2---------------")
 print(net.debug_str())
-print("--------------Ends-----------------")
+print("--------------Ends2----------------")
 
-
-
-# NNVM.compiler.build
+print("-------------Starts3---------------")
+# NNVM-compiler build
 opt_level = 0
-#target = tvm.target.intel_graphics()
-target = tvm.target.create('opencl')
+target = tvm.target.create('llvm')
 with nnvm.compiler.build_config(opt_level=opt_level):
     graph, lib, params = nnvm.compiler.build(net, target, shape={"data": data_shape}, params=params)
-
+print("--------------Ends3----------------")
 
 # create random input
-#ctx = tvm.opencl()
-#data = np.random.uniform(-1, 1, size=data_shape).astype("float32")
+ctx = tvm.context("llvm", 0)
+real_data = np.random.uniform(-1, 1, size=data_shape).astype("float32")
+# create module
+module = graph_runtime.create(graph, lib, ctx)
+# set input and parameters
+module.set_input("data", real_data)
+module.set_input("sparse_kernel", np.random.randint(0, 1, sparse_kernel_shape))
+module.set_input(**params)
+# run
+module.run()
+# get output
+out = module.get_output(0, tvm.nd.empty(out_shape))
+# convert to numpy
+out.asnumpy()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-
-
-# Check computation
-def forward(x, conv_kernel, sparse_kernel, kernel, pad, stride, **args):
-    ishape = x.shape
-    return
-
-def backward(head_grads, x, conv_kernel, sparse_kernel, kernel, pad, stride, **args):
-    return
-
-
-
-dtype = "float32"
-shape = {'x': (1,1,3,3)}
-localtime = time.asctime( time.localtime(time.time()) )
-print("Start time:" + localtime)
-for _ in range(1):
-    check_function(y, forward=None, backward=None, numerical_grads=False, values=np.ones(shape['x'],dtype), dtype=dtype, shape=shape, additional_params={'kernel':[3,3], 'pad':[0,0],'stride':[1,1]})
-localtime = time.asctime( time.localtime(time.time()) )
-print("End time:" + localtime)
-'''
-
-
+# Print first 10 elements of output
+print(out.asnumpy().flatten()[0:10])
